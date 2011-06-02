973f3f1018cd82bb5311f7d8c14f54e42b40412a
Blake Ramsdell <blaker@gmail.com>
2011-06-02 14:45:47 -0700

    Initial revision.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Prototype.Fez.BootloaderUtil
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
