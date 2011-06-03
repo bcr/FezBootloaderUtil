a8e9d499375a606fbc103e1ed12b3c33525b3972
Blake Ramsdell <blaker@gmail.com>
2011-06-02 18:43:36 -0700

    Some more work. Auto-finding the COM port is working now.

---
+++ FezBootloaderController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO.Ports;
+using System.Management;
+
+namespace Prototype.Fez.BootloaderUtil
+{
+    class FezBootloaderController
+    {
+        private SerialPort fezPort;
+
+        internal void Open()
+        {
+            fezPort = OpenFezBootloaderSerialPort();
+            fezPort.Open();
+            fezPort.NewLine = "\r\n";
+        }
+
+        // http://www.codeproject.com/Messages/3692563/Get-correct-driver-name-from-Device-management-ass.aspx
+        // http://geekswithblogs.net/PsychoCoder/archive/2008/01/25/using_wmi_in_csharp.aspx
+
+        private SerialPort OpenFezBootloaderSerialPort()
+        {
+            var searcher = new ManagementObjectSearcher("select DeviceID,MaxBaudRate from Win32_SerialPort where Description = \"GHI Boot Loader Interface\"");
+            foreach (var obj in searcher.Get())
+            {
+                return new SerialPort((string) obj["DeviceID"], (int) (uint) obj["MaxBaudRate"]);
+            }
+            throw new Exception("Unable to find FEZ device. Is it in bootloader mode?");
+        }
+
+        enum FezCommand : byte
+        {
+            None = 0,
+            GetLoaderVersion = (Byte) 'V',
+        }
+
+        internal string GetLoaderVersion()
+        {
+            SendCommand(FezCommand.GetLoaderVersion);
+            var response = fezPort.ReadLine();
+            fezPort.ReadLine(); // Eat trailing BL\r\n
+            return response;
+        }
+
+        private void SendCommand(FezCommand fezCommand)
+        {
+            fezPort.Write(new byte[] { (byte)fezCommand }, 0, 1);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,7 +1,57 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Management;
 using System.Text;
+using System.IO.Ports;
+
+//Availability 2
+//Binary True
+//Capabilities
+//CapabilityDescriptions
+//Caption GHI Boot Loader Interface (COM5)
+//ConfigManagerErrorCode 0
+//ConfigManagerUserConfig False
+//CreationClassName Win32_SerialPort
+//Description GHI Boot Loader Interface
+//DeviceID COM5
+//ErrorCleared
+//ErrorDescription
+//InstallDate
+//LastErrorCode
+//MaxBaudRate 115200
+//MaximumInputBufferSize 0
+//MaximumOutputBufferSize 0
+//MaxNumberControlled
+//Name GHI Boot Loader Interface (COM5)
+//OSAutoDiscovered True
+//PNPDeviceID USB\VID_1B9F&PID_0104\5&35EDA8E7
+//PowerManagementCapabilities System.UInt16[]
+//PowerManagementSupported False
+//ProtocolSupported
+//ProviderType Modem Device
+//SettableBaudRate True
+//SettableDataBits True
+//SettableFlowControl True
+//SettableParity True
+//SettableParityCheck True
+//SettableRLSD True
+//SettableStopBits True
+//Status OK
+//StatusInfo 3
+//Supports16BitMode False
+//SupportsDTRDSR True
+//SupportsElapsedTimeouts True
+//SupportsIntTimeouts True
+//SupportsParityCheck True
+//SupportsRLSD True
+//SupportsRTSCTS False
+//SupportsSpecialCharacters False
+//SupportsXOnXOff False
+//SupportsXOnXOffSet False
+//SystemCreationClassName Win32_ComputerSystem
+//SystemName BLAKKAKE
+//TimeOfLastReset
 
 namespace Prototype.Fez.BootloaderUtil
 {
@@ -9,6 +59,10 @@ namespace Prototype.Fez.BootloaderUtil
     {
         static void Main(string[] args)
         {
+            var controller = new FezBootloaderController();
+            controller.Open();
+            System.Console.WriteLine("Version is {0} so there", controller.GetLoaderVersion());
+            System.Console.WriteLine("All done.");
         }
     }
 }
